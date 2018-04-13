1e7f737b2b255711ea908a4ca692332281a61c9a
DIR\max.f.de.oliveira <max.f.de.oliveira@accenture.com>
2018-04-13 17:45:40 -0300

    .

--- test.py
+++ test.py
@@ -1,11 +1,32 @@
-class Interceptor(object):
+import inspect
+from functools import wraps
 
-    def __getattribute__(self,name):
-        attr = object.__getattribute__(self, name)
-        if hasattr(attr, '__call__') and attr.__name__ == "func":
-            def cached_return(*args, **kwargs):
-                return "cache hit! {0}".format(attr.__name__)
 
-            return cached_return
-        else:
-            return attr
+def interceptor(f):
+    def cached(cached_data=None):
+        return "cached data: {0}".format(cached_data)
+
+    def get_cache(func):
+        return "cached data" if func.__name__ == "func2" else None
+
+    @wraps(f)
+    def wrapper(self, *method_args, **method_kwargs):
+        c = get_cache(f)
+        return cached(cached_data=c) if c is not None else f(self, *method_args, **method_kwargs)
+
+    return wrapper
+
+
+class Test:
+    @interceptor
+    def func(self, data):
+        return "func1 {0}".format(data)
+
+    @interceptor
+    def func2(self):
+        return 'func2'
+
+if __name__ == "__main__":
+    test = Test()
+    print(test.func(2))
+    print(test.func2())
